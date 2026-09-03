9bde01ca4bdf76c4337bcaef655ef02b38e994e8
learnbit <wilsonbalderrama@gmail.com>
2026-09-03 18:37:01 -0400

    feat: improve event navigation and detail page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import { events } from "@/data/events";
+import { formatDate } from "@/utils/date";
+import Image from "next/image";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 type EventDetailProps = {
@@ -15,10 +18,27 @@ export default async function EventPage({ params }: EventDetailProps) {
   }
 
   return (
-    <div>
-      Title: {event.title}
-      Category: {event.category}
-      Location: {event.location}
+    <div className="max-w-4xl mx-auto px-4 py-4">
+      <div className="py-4">
+        <Link className="text-muted hover:text-foreground" href="/">
+          ← Volver a eventos
+        </Link>
+      </div>
+      <div className="w-full relative aspect-video overflow-hidden rounded-md">
+        <Image
+          className="object-cover"
+          src={event.image}
+          alt={"event image detail"}
+          fill
+        />
+      </div>
+      <div className="flex flex-col gap-2 py-2">
+        <p className="text-2xl font-semibold">{event.title}</p>
+        <p className="text-sm text-muted">{event.category}</p>
+        <p className="text-sm text-muted">{formatDate(event.date)}</p>
+        <p className="text-sm text-muted">{event.location}</p>
+        <p className="mt-2">{event.description}</p>
+      </div>
     </div>
   );
 }
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -11,16 +11,17 @@ export default function EventCard({ event }: EventCardProps) {
   const { title, category, location, date, slug, image } = event;
 
   return (
-    <div className="bg-surface border border-border rounded-md p-4 w-full space-y-2">
+    <Link
+      href={`/events/${slug}`}
+      className="bg-surface border border-border rounded-md p-4 w-full space-y-2 hover:bg-surface-hover"
+    >
       <div className="w-full relative bg-surface-hover rounded-md mb-4 aspect-video overflow-hidden">
         <Image className="object-cover" src={image} alt={"event image"} fill />
       </div>
-      <h2 className="text-lg font-semibold">
-        <Link href={`/events/${slug}`}>{title}</Link>
-      </h2>
+      <h2 className="text-lg font-semibold">{title}</h2>
       <p className="text-sm">{category}</p>
       <p className="text-muted">{location}</p>
       <p className="text-muted">{formatDate(date)}</p>
-    </div>
+    </Link>
   );
 }
